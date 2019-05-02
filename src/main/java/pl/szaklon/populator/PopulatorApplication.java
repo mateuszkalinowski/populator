d49257492f407ad2a96c606b061725e3bfdb7107
Mateusz Kalinowski <mateusz.kalinowski@icloud.com>
2019-05-02 12:14:01 +0200

    Initial commit - Empty Spring Boot App

---
+++ src/main/java/pl/szaklon/populator/PopulatorApplication.java
@@ -0,0 +1,13 @@
+package pl.szaklon.populator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopulatorApplication.class, args);
+    }
+
+}
